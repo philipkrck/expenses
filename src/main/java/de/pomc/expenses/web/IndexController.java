f62e91e280dc948ae5837bf0a1ffaf56e7e8a015
Philip Krück <philip.krueck@apploft.de>
2020-06-29 16:12:58 +0200

    added get request parameter mapping

--- src/main/java/de/pomc/expenses/web/IndexController.java
+++ src/main/java/de/pomc/expenses/web/IndexController.java
@@ -1,13 +1,18 @@
 package de.pomc.expenses.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, @RequestParam(name = "hello", required = false) String hello) {
+        if (hello != null) {
+            model.addAttribute("greeting", "Hello, " + hello + "!");
+        }
         return "index";
     }
 }
