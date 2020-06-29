e14e36b1d5ba4f545c566c4915bc6b4c0c23a419
Philip Krück <philip.krueck@apploft.de>
2020-06-29 15:16:34 +0200

    initial commit

---
+++ src/main/java/de/pomc/expenses/ExpensesApplication.java
@@ -0,0 +1,13 @@
+package de.pomc.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesApplication.class, args);
+	}
+
+}
