8264d5b890eaa03cf9d3054fd3fd216138c72c9f
Philip Krück <philip.krueck@apploft.de>
2020-06-30 14:40:27 +0200

    created starting point for Expenses application

--- src/main/java/de/pomc/expenses/web/IndexController.java
+++ src/main/java/de/pomc/expenses/web/IndexController.java
@@ -1,18 +1,18 @@
 package de.pomc.expenses.web;
 
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.DocFlavor;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model, @RequestParam(name = "hello", required = false) String hello) {
-        if (hello != null) {
-            model.addAttribute("greeting", "Hello, " + hello + "!");
-        }
+    public String showUser() {
         return "index";
     }
+
 }
